6f2d64679b4f03e92e251f85c2862002ec49ce31
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-03-26 11:36:01 +0100

    Added Spring support
    Created a spring context in the Core module and use Dependency Injection for services

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -0,0 +1,146 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Game {
+    private final GameData gameData = new GameData();
+    private final World world = new World();
+    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
+    private final Pane gameWindow = new Pane();
+    private final Text scoreText = new Text(10, 20, "Destroyed asteroids: 0");
+    private Collection<IEntityProcessingService> entityProcessingServices;
+    private Collection<IPostEntityProcessingService> postEntityProcessingServices;
+    private Collection<IGamePluginService> gamePluginServices;
+
+    public Game(Collection<IEntityProcessingService> entityProcessingServices,
+                Collection<IPostEntityProcessingService> postEntityProcessingServices,
+                Collection<IGamePluginService> gamePluginServices) {
+        this.entityProcessingServices = entityProcessingServices;
+        this.postEntityProcessingServices = postEntityProcessingServices;
+        this.gamePluginServices = gamePluginServices;
+    }
+
+    public void start(Stage window) throws Exception {
+        //Text text = new Text(10, 20, "Destroyed asteroids: 0");
+        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        gameWindow.getChildren().add(scoreText);
+
+        gameWindow.heightProperty().addListener(((observableValue, oldValue, newValue) -> {
+            gameData.setDisplayHeight(newValue.intValue());
+        }));
+
+        gameWindow.widthProperty().addListener(((observableValue, oldValue, newValue) -> {
+            gameData.setDisplayWidth(newValue.intValue());
+        }));
+
+        Scene scene = new Scene(gameWindow);
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, true);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, true);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, true);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, true);
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, false);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, false);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, false);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, false);
+            }
+
+        });
+
+        // Lookup all Game Plugins using ServiceLoader
+        for (IGamePluginService iGamePlugin : gamePluginServices) {
+            iGamePlugin.start(gameData, world);
+        }
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
+            polygon.setFill(Color.rgb(entity.getColor()[0] % 255, entity.getColor()[1] % 255, entity.getColor()[2] % 255));
+            polygons.put(entity, polygon);
+            gameWindow.getChildren().add(polygon);
+        }
+        render();
+        window.setScene(scene);
+        window.setTitle("ASTEROIDS");
+        window.show();
+    }
+
+    public void render() {
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+                draw();
+                gameData.getKeys().update();
+            }
+
+        }.start();
+    }
+
+    private void update() {
+        for (IEntityProcessingService entityProcessorService : entityProcessingServices) {
+            entityProcessorService.process(gameData, world);
+        }
+        for (IPostEntityProcessingService postEntityProcessorService : postEntityProcessingServices) {
+            postEntityProcessorService.process(gameData, world);
+        }
+    }
+
+    private void draw() {
+        for (Entity polygonEntity : polygons.keySet()) {
+            if(!world.getEntities().contains(polygonEntity)){
+                Polygon removedPolygon = polygons.get(polygonEntity);
+                polygons.remove(polygonEntity);
+                gameWindow.getChildren().remove(removedPolygon);
+            }
+        }
+
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = polygons.get(entity);
+            if (polygon == null) {
+                polygon = new Polygon(entity.getPolygonCoordinates());
+                polygon.setFill(Color.rgb(entity.getColor()[0] % 255, entity.getColor()[1] % 255, entity.getColor()[2] % 255));
+                polygons.put(entity, polygon);
+                gameWindow.getChildren().add(polygon);
+            }
+            polygon.setTranslateX(entity.getX());
+            polygon.setTranslateY(entity.getY());
+            polygon.setRotate(entity.getRotation());
+        }
+
+        scoreText.setText("Destroyed asteroids: " + gameData.getAsteroidsKilled());
+    }
+}
