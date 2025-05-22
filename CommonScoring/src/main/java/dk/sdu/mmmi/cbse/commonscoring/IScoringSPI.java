47517edebeb8ef635197ddf54f30f4982106d653
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-05-22 20:43:24 +0200

    Added JavaDoc for IScoringSPI

--- CommonScoring/src/main/java/dk/sdu/mmmi/cbse/commonscoring/IScoringSPI.java
+++ CommonScoring/src/main/java/dk/sdu/mmmi/cbse/commonscoring/IScoringSPI.java
@@ -1,5 +1,14 @@
 package dk.sdu.mmmi.cbse.commonscoring;
 
+/**
+ * SPI for scoring systems.
+ */
 public interface IScoringSPI {
+    /**
+     * Method for updating and retrieving the current score
+     *
+     * @param points The amount of point to increase the score by
+     * @return The current score
+     */
     int updateScore(int points);
 }
