1b480d3d17fa28c1a60598caa389db6e806c072b
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-05-24 16:08:35 +0200

    More JavaDoc

--- CommonScoring/src/main/java/dk/sdu/mmmi/cbse/commonscoring/IScoringSPI.java
+++ CommonScoring/src/main/java/dk/sdu/mmmi/cbse/commonscoring/IScoringSPI.java
@@ -7,6 +7,11 @@ public interface IScoringSPI {
     /**
      * Method for updating and retrieving the current score
      *
+     * <p>
+     * Precondition - A scoring increasing event must have happened
+     * <p>
+     * Postcondition - The score must have been updated and returned
+     *
      * @param points The amount of point to increase the score by
      * @return The current score
      */
